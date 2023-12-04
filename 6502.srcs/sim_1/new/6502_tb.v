c0c1191d0de42f655de1a56ea4371b0633897400
Sam <sammys.email.home@gmail.com>
2023-12-04 16:26:10 -0600

    Vivado Init

---
+++ 6502.srcs/sim_1/new/6502_tb.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/04/2023 04:06:25 PM
+// Design Name: 
+// Module Name: 6502_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module 6502_tb;
+
+input clock;
+
+endmodule
